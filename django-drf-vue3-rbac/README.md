6187c13f25f97688d9653ca90b8b17273fb70419
养乐多要吸管 <a102467@outlook.com>
2023-07-24 23:33:36 +0800

    vue

--- django-drf-vue3-rbac/README.md
+++ django-drf-vue3-rbac/README.md
@@ -1,4 +1,4 @@
-# django-drf-vue3
+# django-drf-vue3-rbac
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ django-drf-vue3-rbac/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <router-view/>
+</template>
+
+<style>
+
+</style>
