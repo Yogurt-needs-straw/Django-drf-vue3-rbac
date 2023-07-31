f88cc97e2c7926f6521453605cfdcfc21af72962
养乐多要吸管 <a102467@outlook.com>
2023-07-31 23:52:36 +0800

    Backend permission verification

--- Django_drf_vue3_project/api/views.py
+++ Django_drf_vue3_project/api/views.py
@@ -1,8 +1,61 @@
 from django.shortcuts import render
+from rest_framework.permissions import BasePermission
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
+from rest_framework.authentication import BaseAuthentication
 
 from api import models
+
+class User(object):
+    def __init__(self, name=None, role=None):
+        self.name = name
+        self.role = role
+
+# 认证组件
+class MineAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        # 读取用户请求token，校验是否合法
+        token = request.query_params.get('token')
+        role = request.query_params.get('role')
+        if not token:
+            raise exceptions.AuthenticationFailed("认证失败")
+
+        # request.user.name
+        # request.user.role
+        # request.auth
+        return (User("xdd", role), None)
+
+
+
+    def authenticate_header(self, request):
+        return "API"
+
+# 权限校验
+class MinePermission(BasePermission):
+    def has_permission(self, request, view):
+        # print("判断权限", request.user)
+        # 1.当前用户所有的权限
+        from django.conf import settings
+        permission_dict = settings.PERMISSIONS[request.user.role]
+
+        # 2.当前用户正在访问的url和方式
+        # print(request.resolver_match.url_name, request.method)
+        url_name = request.resolver_match.url_name
+        method = request.method
+
+        # 3.权限判断
+        method_list = permission_dict.get(url_name)
+        if not method_list:
+            return False
+
+        if method in method_list:
+            return True
+
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        # 视图中 self.get_object()
+        return True
 
 class DepartSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,5 +65,8 @@ class DepartSerializer(serializers.ModelSerializer):
 class DepartView(ModelViewSet):
     queryset = models.Depart.objects.all()
     serializer_class = DepartSerializer
+
+    authentication_classes = [MineAuthentication, ]
+    permission_classes = [MinePermission, ]
 
 
